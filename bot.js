699657a3a8e5834413099c02474c0c233a779aea
EnesOnurAta <38593480+EnesOnurAta@users.noreply.github.com>
2018-05-06 16:57:05 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,7 +17,7 @@ client.on('message', msg => {
   }
 });
 client.on('message', msg => {
-  if (msg.content === '@Zappara TEST') {
+  if (msg.content === 'Zappara TEST') {
     msg.reply('Efendim');
   }
 });
