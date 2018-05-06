a8196793bee11ad601c2e6e6fc8a4505c9700397
EnesOnurAta <38593480+EnesOnurAta@users.noreply.github.com>
2018-05-06 16:50:07 +0300

    Update sunucu.js

--- ozellikler/sunucu.js
+++ ozellikler/sunucu.js
@@ -1,12 +1,12 @@
 const Discord = require('discord.js');
 exports.run = (client, message, args) => {
     if (msg.content === prefix + 'sunucu') {
-    const embed = new Discord.RichEmbed()
-      .setColor("RANDOM")
-      .setThumbnail(msg.author.avatarURL)
-      .addField("Sunucu Sahibi", msg.guild.owner)
-      .addField("Toplam Üye Sayısı", msg.guild.members.size)
-      .addField("Oluşturulma Tarihi", msg.guild.createdAt)
-    msg.channel.send(embed)
-  }
-});
+        const embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+         .setThumbnail(msg.author.avatarURL)
+         .addField("Sunucu Sahibi", msg.guild.owner)
+         .addField("Toplam Üye Sayısı", msg.guild.members.size)
+         .addField("Oluşturulma Tarihi", msg.guild.createdAt)
+        msg.channel.send(embed)
+        }
+    });
