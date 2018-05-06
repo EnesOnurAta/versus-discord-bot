1a21dc5d57c199bde235aa37912b6dc887d4b7fb
EnesOnurAta <38593480+EnesOnurAta@users.noreply.github.com>
2018-05-06 16:46:35 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,51 +21,6 @@ client.on('message', msg => {
     msg.reply('Efendim');
   }
 });
-client.on('message', msg => {
-  if (msg.content === '@Zappara TEST#2406 yardım') {
-    const embed = new Discord.RichEmbed()
-      .setColor("RED")
-      .setAuthor(msg.author.username, msg.author.avatarURL)
-      .addField("KOMUTLAR", `${prefix} benimid = Sizin ID'nizi gösterir` `${prefix} ping = Sizin pinginizi gösterir`)
-      .setDescription(`${bot.user.username} KOMUTLAR`)
-      .setFooter("Zappara TEST")
-      .setTimestamp()
-    const embed2 = new Discord.RichEmbed()
-      .setColor("GREEN")
-      .setDescription("Özel mesajlarını kontrol et :postbox:")
-      .setAuthor(msg.author.username, msg.author.avatarURL)
-      .setFooter("Zappara TEST")
-      .setTimestamp()
-    msg.channel.send(embed2).then(msg.author.send(embed));
-  }
-});
-if (msg.content === prefix + 'yardım') {
-    const embed = new Discord.RichEmbed()
-      .setColor("RED")
-      .setAuthor(msg.author.username, msg.author.avatarURL)
-      .addField("KOMUTLAR", `${prefix} benimid = Sizin ID'nizi gösterir`)
-      .setDescription(`${bot.user.username} KOMUTLAR`)
-      .setFooter("Zappara TEST")
-      .setTimestamp()
-    const embed2 = new Discord.RichEmbed()
-      .setColor("GREEN")
-      .setDescription("Özel mesajlarını kontrol et :postbox:")
-      .setAuthor(msg.author.username, msg.author.avatarURL)
-      .setFooter("Zappara TEST")
-      .setTimestamp()
-    msg.channel.send(embed2).then(msg.author.send(embed));
-  }
-  if (msg.content === prefix + 'sunucu') {
-    const embed = new Discord.RichEmbed()
-      .setColor("RANDOM")
-      .setThumbnail(msg.author.avatarURL)
-      .addField("Sunucu Sahibi", msg.guild.owner)
-      .addField("Toplam Üye Sayısı", msg.guild.members.size)
-      .addField("Oluşturulma Tarihi", msg.guild.createdAt)
-    msg.channel.send(embed)
-  }
-});
-
 
 
 
